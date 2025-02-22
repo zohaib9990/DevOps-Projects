fe93da41a77a47a1b47e414dff9ad5fa46819b27
Muhammad Zohaib Hussain <144581164+zohaib9990@users.noreply.github.com>
2025-02-22 16:59:35 +0500

    Update backend.tf

--- DevOps-Project-28/Chatbot-UI/EKS-TF/backend.tf
+++ DevOps-Project-28/Chatbot-UI/EKS-TF/backend.tf
@@ -1,16 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "new-tetris-bucket"
+    bucket         = "new-tetris-bucket-123456789012"
     region         = "us-east-1"
     key            = "Chatbot-UI/EKS-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
-  }
-  required_version = ">=0.13.0"
-  required_providers {
-    aws = {
-      version = ">= 2.7.0"
-      source  = "hashicorp/aws"
-    }
   }
 }
